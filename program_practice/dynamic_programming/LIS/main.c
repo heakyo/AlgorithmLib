074412ac89a8d27f22b866512c7972ab04999eeb
Marvin <heakyo163@163.com>
2016-10-09 17:40:33 +0800

    dynamic programming: LIS----status: development
    Signed-off-by: Marvin <heakyo163@163.com>

---
+++ program_practice/dynamic_programming/LIS/main.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  Dynamic Programming
+ *    				Longest Increasing Subsequence
+ *
+ *        Version:  1.0
+ *        Created:  2016年10月09日 14时33分00秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
